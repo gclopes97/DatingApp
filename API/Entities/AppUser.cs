f490342cb6c2d1d7d02d66342c2e415a3ea9f633
Gilberto Lopes <gclopes97@hotmail.com>
2021-03-10 15:23:15 -0300

    End of section 9

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -10,7 +10,7 @@ namespace API.Entities
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
-        public DateTime DateofBirth { get; set; } 
+        public DateTime DateOfBirth { get; set; } 
         public string KnownAs { get; set; } 
         public DateTime Created { get; set; } = DateTime.Now;
         public DateTime LastActive { get; set; } = DateTime.Now;
@@ -24,7 +24,8 @@ namespace API.Entities
 
         // public int GetAge()
         // {
-        //     return DateofBirth.CalculateAge();
+        //      return DateofBirth.CalculateAge();
+
         // }
     }
 }
