d231330a22eed6785c6a922a79f3ee1854c0765a
gclopes97 <gclopes97@hotmail.com>
2021-09-14 19:18:30 -0300

    Section 19

--- API/Controllers/FallBackController.cs
+++ API/Controllers/FallBackController.cs
@@ -3,12 +3,12 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    public class FallBackController : Controller
+    public class FallbackController : Controller
     {
         public ActionResult Index()
         {
             return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(), 
-                "wwwroot", "index.html"), "text/html");
+                "wwwroot", "index.html"), "text/HTML");
         }
     }
 }
--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -22,20 +22,21 @@ namespace API.Middleware
             _next = next;
         }
 
-        public async Task InvokeAsync(HttpContext context){
+        public async Task InvokeAsync(HttpContext context)
+        {
             try
             {
                 await _next(context);
             }
             catch (Exception ex)
-            {                
+            {
                 _logger.LogError(ex, ex.Message);
                 context.Response.ContentType = "application/json";
                 context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
 
                 var response = _env.IsDevelopment()
-                ? new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString())
-                : new ApiException(context.Response.StatusCode, "Internal Server Error");
+                    ? new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace?.ToString())
+                    : new ApiException(context.Response.StatusCode, "Internal Server Error");
 
                 var options = new JsonSerializerOptions{PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
 
